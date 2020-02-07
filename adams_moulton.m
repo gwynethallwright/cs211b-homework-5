fdf2332baec383d522b82635a693921fcb8aff6f
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-02-07 11:42:54 -0800

    Fix bugs in numerical evolution. Code solves first problem.

--- adams_moulton.m
+++ adams_moulton.m
@@ -1,4 +1,5 @@
 % t refers to the time at the earliest point we consider
+% This is one step further in time than for Bashford
 function u = adams_moulton(y_0, y_guess, h, t, fcnHandle)
-    u = y_0 + 0.5*h*fcnHandle(y_guess, t+h) + fcnHandle(y_0, t);
+    u = y_0 + 0.5*h*(fcnHandle(y_guess, t+h) + fcnHandle(y_0, t));
 end
--- calculate_next.m
+++ calculate_next.m
@@ -1,4 +1,4 @@
 function u = calculate_next(y, h, t, fcnHandle)
     y_guess = adams_bashford(y, h, t, fcnHandle);
-    u = adams_moulton(y(:,1), y_guess, h, t, fcnHandle);
+    u = adams_moulton(y(:,2), y_guess, h, t+h, fcnHandle);
 end
