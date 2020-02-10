16db6c0975af8a03efffbea82832f01893671306
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-02-10 15:01:18 -0800

    Change loop structure to accommodate redoing an iteration with a smaller step size.

--- calculate_next.m
+++ calculate_next.m
@@ -3,7 +3,11 @@ function [u, r] = calculate_next(y, h, t, fcnHandle, tol)
     u = adams_moulton(y(:,2), y_guess, h, t+h, fcnHandle);
     % Estimate for local truncation error
     C = 1.0/12.0;
+    frac = 0.9;
     lte = norm(C*power(h, 3)*power(u, 3), inf);
-    r = power(0.9*tol/lte, 1/3);
-    disp(r);
+    if lte >= frac*tol
+        r = power(frac*tol/lte, 1.0/3.0);
+    else
+        r = 1;
+    end
 end
