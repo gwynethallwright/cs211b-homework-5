5651097428a9e611eaa46588a9651a1806ecbc6c
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-02-10 19:20:17 -0800

    Full support for variable step size.

--- evolve_ode.m
+++ evolve_ode.m
@@ -1,20 +1,27 @@
-function y = evolve_ode(h, t_0, t_f, y_0, y_1, fcnHandle, tol)
-    n_steps = (t_f-t_0)/h;
-    y = zeros(2, n_steps+1);
+function [y, t] = evolve_ode(h, t_0, t_f, y_0, y_1, fcnHandle, tol)
+    % Array to store solution
+    % Note: we overestimate the array size
+    y = nan(2, 1e3);
+    % Array to store points at which solution is sampled
+    t = nan(1, 1e3);
+    % Add initial values
     y(:,1) = y_0;
     y(:,2) = y_1;
+    t(1) = t_0;
+    t(2) = t_0 + h;
+    % Keep track of index in array
     i = 3;
-    while i <= n_steps+1
+    t_current = t_0;
+    while t_current < t_f
         % The function calculate_next_theta returns the desired solution at  
-        % the next time step using the theta method 
-        % It is a predictor-corrector method, so it first calls the 
-        % Newton iterator to approximate the solution at the next step, 
-        % after which it uses the theta method to correct the solution
-        [y(:,i), r] = calculate_next(y(:,i-2:i-1), h, t_0+i*h, fcnHandle, tol);
+        % the next time step using AB as the predictor and AM as the
+        % corrector
+        [y(:,i), r] = calculate_next(y(:,i-2:i-1), h, t(i-1), fcnHandle, tol);
         if r == 1
+            t(i) = t(i-1) + h;
+            t_current = t_current + h;
             i = i+1;
-        else
-            h = r*h;
         end
+        h = r*h;
     end
 end
--- main.m
+++ main.m
@@ -13,8 +13,9 @@ y_1 = forward_euler_step(y_0, h, t_0, @f_1);
 for i = 1:numel(h)
     % The function evolve_ode returns the desired solution at all times 
     % given the initial condition, start time, end time and step size
-    y = evolve_ode(h(i), t_0, t_f, y_0, y_1, @f_1, tol);
-    t = 0:h(i):t_f;
+    [y, t] = evolve_ode(h(i), t_0, t_f, y_0, y_1, @f_1, tol);
+    y = y(:,~isnan(t));
+    t = t(~isnan(t));
     figure;
     % Plot and format plot
     plot(t, y(1,:));
