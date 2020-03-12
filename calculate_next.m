d57ce8b2975538661f69101edcf1562688358660
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-03-12 16:18:18 -0700

    Thank you Alexgit add evolve_ode.m

--- calculate_next.m
+++ calculate_next.m
@@ -1,4 +1,4 @@
-function [u, r] = calculate_next(y, h, t, fcnHandle, tol)
+function [u, r, redo] = calculate_next(y, h, t, fcnHandle, tol)
     y_guess = adams_bashford(y, h, t, fcnHandle);
     u = adams_moulton(y(:,2), y_guess, h, t+h, fcnHandle);
     % Estimate for local truncation error
@@ -6,9 +6,12 @@ function [u, r] = calculate_next(y, h, t, fcnHandle, tol)
     lte = 1.0/6.0 * norm(y_guess-u, inf);
     % If the LTE exceeds some fraction of the tolerance, compute r to
     % adjust the step size
+    r = power(frac*tol/lte, 1.0/3.0);
     if lte >= frac*tol
-        r = power(frac*tol/lte, 1.0/3.0);
+        % We do not accept this step
+        redo = 1;
     else
-        r = 1;
+        % We accept this step
+        redo = 0;
     end
 end
