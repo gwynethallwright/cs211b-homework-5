de7478841e7c64445e0769e392ba8a96e45164dd
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-03-12 15:33:52 -0700

    Modify LTE for predictor-corrector.

--- calculate_next.m
+++ calculate_next.m
@@ -4,7 +4,8 @@ function [u, r] = calculate_next(y, h, t, fcnHandle, tol)
     % Estimate for local truncation error
     C = 1.0/12.0;
     frac = 0.9;
-    lte = norm(C*power(h, 3)*power(u, 3), inf);
+    % lte = norm(C*power(h, 3)*power(u, 3), inf);
+    lte = 1.0/6.0 * norm(y_guess-u, inf);
     % If the LTE exceeds some fraction of the tolerance, compute r to
     % adjust the step size
     if lte >= frac*tol
