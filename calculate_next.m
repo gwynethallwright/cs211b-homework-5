2a0907768486c8c61ece1a864cb06e8667b3fd7f
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-03-12 15:55:55 -0700

    More LTE changes.

--- calculate_next.m
+++ calculate_next.m
@@ -2,9 +2,7 @@ function [u, r] = calculate_next(y, h, t, fcnHandle, tol)
     y_guess = adams_bashford(y, h, t, fcnHandle);
     u = adams_moulton(y(:,2), y_guess, h, t+h, fcnHandle);
     % Estimate for local truncation error
-    C = 1.0/12.0;
     frac = 0.9;
-    % lte = norm(C*power(h, 3)*power(u, 3), inf);
     lte = 1.0/6.0 * norm(y_guess-u, inf);
     % If the LTE exceeds some fraction of the tolerance, compute r to
     % adjust the step size
--- calculate_next_4.m
+++ calculate_next_4.m
@@ -2,9 +2,8 @@ function [u, r] = calculate_next_4(y, dy, dt, m, fcnHandle, tol)
     y_guess = adams_bashford_4(y, dy, dt, m, fcnHandle);
     u = adams_moulton_4(y(:,2), y_guess, dy, dt, m, fcnHandle);
     % Estimate for local truncation error
-    C = 1.0/12.0;
     frac = 0.9;
-    lte = norm(C*power(dt, 3)*power(u, 3), inf);
+    lte = 1.0/6.0 * norm(y_guess-u, inf);
     % If the LTE exceeds some fraction of the tolerance, compute r to
     % adjust the step size
     if lte >= frac*tol
