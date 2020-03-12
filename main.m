b5035076f8ca55978ab4e33ec2efb9c36f3cd0ae
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-03-12 15:34:07 -0700

    Format plots.

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 % Select initial step sizes
 h = 0.01;
 % Select tolerance
-tol = 1e-6;
+tol = 1e-3;
 % Select initial and final times
 t_0 = 0;
 t_f = 1;
@@ -22,41 +22,61 @@ w_1 = forward_euler_step(w_0, h, t_0, @f_3);
 % Evolve and plot solutions
 % The function evolve_ode returns the desired solution at all times 
 % given the initial condition, start time, end time and initial step size
-% % PROBLEM 1
-% [y, t, steps] = evolve_ode(h, t_0, t_f, y_0, y_1, @f_1, tol);
-% figure;
-% plot(t, y(1,:));
-% hold on;
-% plot(t, y(2,:));
-% legend({'$y_1$','$y_2$'},'Interpreter', 'latex');
-% ax = gca;
-% ax.FontSize = 20;
-% xlabel('$t$', 'Interpreter', 'latex');
-% ylabel('$y$', 'Interpreter', 'latex');
-% title('Numerical Evolution of $y$', 'Interpreter', 'latex');
-% xlim([0, 1]);
-% % PROBLEM 2
-% [u, t_2, steps] = evolve_ode(h, t_0, t_f_2, u_0, u_1, @f_2, tol);
-% figure;
-% plot(t_2, u(1,:));
-% hold on;
-% plot(t_2, u(2,:));
-% legend({'$y_1$','$y_2$'},'Interpreter', 'latex');
-% ax = gca;
-% ax.FontSize = 20;
-% xlabel('$t$', 'Interpreter', 'latex');
-% ylabel('$y$', 'Interpreter', 'latex');
-% title('Numerical Evolution of $y$', 'Interpreter', 'latex');
-% xlim([0, 100]);
-% figure;
-% plot(u(1,:), (u(2,:)));
-% ax = gca;
-% ax.FontSize = 20;
-% xlabel('$y_1$', 'Interpreter', 'latex');
-% ylabel('$y_2$', 'Interpreter', 'latex');
-% title('Numerical Evolution of $y$', 'Interpreter', 'latex');
+
+% PROBLEM 1
+[y, t, steps] = evolve_ode(h, t_0, t_f, y_0, y_1, @f_1, tol);
+figure;
+plot(t, y(1,:));
+hold on;
+plot(t, y(2,:));
+legend({'$y_1$','$y_2$'},'Interpreter', 'latex');
+ax = gca;
+ax.FontSize = 20;
+xlabel('$t$', 'Interpreter', 'latex');
+ylabel('$y$', 'Interpreter', 'latex');
+title('Numerical Evolution of $y$', 'Interpreter', 'latex');
+xlim([0, 1]);
+figure;
+plot(t, steps);
+ax = gca;
+ylim([0, 5e-2]);
+xlim([0, 1]);
+ax.FontSize = 20;
+xlabel('$t$', 'Interpreter', 'latex');
+ylabel('$h$', 'Interpreter', 'latex');
+title('Step Size $h$ over Time $t$', 'Interpreter', 'latex');
+
+% PROBLEM 2
+[u, t_2, steps_2] = evolve_ode(h, t_0, t_f_2, u_0, u_1, @f_2, tol);
+figure;
+plot(t_2, u(1,:));
+hold on;
+plot(t_2, u(2,:));
+legend({'$y_1$','$y_2$'},'Interpreter', 'latex');
+ax = gca;
+ax.FontSize = 20;
+xlabel('$t$', 'Interpreter', 'latex');
+ylabel('$y$', 'Interpreter', 'latex');
+title('Numerical Evolution of $y$', 'Interpreter', 'latex');
+xlim([0, 100]);
+figure;
+plot(u(1,:), (u(2,:)));
+ax = gca;
+ax.FontSize = 20;
+xlabel('$y_1$', 'Interpreter', 'latex');
+ylabel('$y_2$', 'Interpreter', 'latex');
+title('Numerical Evolution of $y$', 'Interpreter', 'latex');
+plot(t_2, steps_2);
+ax = gca;
+ax.FontSize = 20;
+xlabel('$t$', 'Interpreter', 'latex');
+ylabel('$h$', 'Interpreter', 'latex');
+title('Step Size $h$ over Time $t$', 'Interpreter', 'latex');
+ylim([0, 5e-2]);
+xlim([0, 100]);
+
 % PROBLEM 3
-[w, t_3, steps] = evolve_ode(h, t_0, t_f_3, w_0, w_1, @f_3, tol);
+[w, t_3, steps_3] = evolve_ode(h, t_0, t_f_3, w_0, w_1, @f_3, tol);
 figure;
 plot(t_3, w(1,:));
 hold on;
@@ -69,10 +89,11 @@ ylabel('$y$', 'Interpreter', 'latex');
 title('Numerical Evolution of $y$', 'Interpreter', 'latex');
 xlim([0, 11]);
 figure;
-plot(t_3, steps);
+plot(t_3, steps_3);
 ax = gca;
 ax.FontSize = 20;
 xlabel('$t$', 'Interpreter', 'latex');
 ylabel('$h$', 'Interpreter', 'latex');
 title('Step Size $h$ over Time $t$', 'Interpreter', 'latex');
+ylim([0, 5e-2]);
 xlim([0, 11]);
