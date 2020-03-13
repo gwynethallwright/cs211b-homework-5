e23307232ebd593e79aaf5bb34fcf15d0df88bbc
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-03-12 18:38:11 -0700

    Fix IC and interpolate to get solutions at off-grip points.

--- main_q4.m
+++ main_q4.m
@@ -1,6 +1,6 @@
 % Select initial step sizes
 dt = 0.01;
-dy = 0.005;
+dy = 0.003;
 % Select tolerance
 tol = 1e-3;
 % Select initial and final times
@@ -9,13 +9,18 @@ t_f = 1;
 % Grid of spatial points
 x = 0:dy:1;
 % Select initial condition for fourth problem
-y_0 = transpose(exp(-x));
+y_0 = transpose(exp(-10*x));
 m = numel(x);
 % Use FEM to get second initial condition for fourth problem
 y_1 = forward_euler_step_4(y_0, dy, dt, m);
 [y, t, steps] = evolve_ode_4(dy, dt, t_0, t_f, y_0, y_1, m, @f_4, tol);
+t_grid = [0, 0.25, 0.5, 0.6, 0.8, 1.0];
+y_grid = interp2(transpose(t), x, y, transpose(t_grid), x);
 figure;
-plot(x, y(:, 80));
+for i = 1:6
+    plot(x, y_grid(:, i)); 
+    hold on;
+end
 ax = gca;
 ax.FontSize = 20;
 xlabel('$x$', 'Interpreter', 'latex');
